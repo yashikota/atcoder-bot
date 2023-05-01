4389d656fd002e5f59aac87e54e45d30be052ec2
kota <52403688+yashikota@users.noreply.github.com>
2023-05-01 20:25:51 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use worker::*;
+
+mod utils;
+
+fn log_request(req: &Request) {
+    console_log!(
+        "{} - [{}], located at: {:?}, within: {}",
+        Date::now().to_string(),
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or_else(|| "unknown region".into())
+    );
+}
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    log_request(&req);
+
+    utils::set_panic_hook();
+
+    let router = Router::new();
+
+    router
+        .get("/", |_, _| {
+            let res = "Atcoder-bot";
+            Response::ok(res)
+        })
+        .run(req, env)
+        .await
+}
